b6e7ca4c96786fdf3d7cab75376f00e9e15e7cc9
Gerold Busch <gerold.busch@valtech.com>
2023-06-10 16:01:10 +0200

    url escaping try 2

--- superset/migrations/env.py
+++ superset/migrations/env.py
@@ -43,8 +43,7 @@ if "sqlite" in DATABASE_URI:
         "SQLite Database support for metadata databases will \
         be removed in a future version of Superset."
     )
-decoded_uri = urllib.parse.unquote(DATABASE_URI)
-config.set_main_option("sqlalchemy.url", decoded_uri)
+config.set_main_option('sqlalchemy.url', DATABASE_URI.replace("%", "%%"))
 target_metadata = Base.metadata  # pylint: disable=no-member
 
 
